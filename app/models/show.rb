838a7e197511c954eef71ccaa0490aad14bfd697
Omar Nava <omarnavaalvarez@gmail.com>
2018-04-02 16:58:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.find_by(rating: self.highest_rating) 
+    Show.find_by(rating: self.highest_rating)
   end
 
   def self.lowest_rating
